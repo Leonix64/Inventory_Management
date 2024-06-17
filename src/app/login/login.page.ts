893646d028010cb831817780a4604c0dd3feb748
Angel Gonzalez <angel.gonzalezh08@gmail.com>
2024-06-17 11:16:07 -0600

    register y login en progreso

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService, LoginData } from '../services/login.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginData: LoginData = {
+    name: '',
+    password: '',
+  };
+
+  constructor(
+    private loginService: LoginService,
+    private toastController: ToastController
+  ) { }
 
   ngOnInit() {
   }
 
+  loginUser() {
+    this.loginService.LoginUser(this.loginData).subscribe(
+      (response) => {
+        this.presentToast('User successfully logged in');
+        console.log(response);
+      },
+      (error) => {
+        this.presentToastError('Review your credentials and try again');
+        console.log(error);
+      }
+    )
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom',
+      color: 'success'
+    });
+    toast.present();
+  }
+
+  async presentToastError(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom',
+      color: 'danger'
+    });
+    toast.present();
+  }
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RegisterService, UserData } from '../services/register.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  userData: UserData = {
+    name: '',
+    last_name: '',
+    age: null,
+    phone: '',
+    gender: '',
+    password: '',
+  };
+
+  constructor(
+    private registerService: RegisterService,
+    private toastController: ToastController
+  ) { }
 
   ngOnInit() {
   }
 
+  registerUser() {
+    this.registerService.registerUser(this.userData).subscribe(
+      (response) => {
+        this.presentToast('User successfully registered');
+        console.log(response);
+      },
+      (error) => {
+        this.presentToastError('Review your credentials and try again');
+        console.log(error);
+      }
+    )
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom',
+      color: 'success'
+    });
+    toast.present();
+  }
+
+  async presentToastError(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom',
+      color: 'danger'
+    });
+    toast.present();
+  }
 }
